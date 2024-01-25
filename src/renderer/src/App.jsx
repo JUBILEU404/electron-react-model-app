69ded3e3be76f6454238170046040575a6a7d29e
JUBILEU404 <locaytonfenix@gmail.com>
2024-01-25 10:01:44 -0300

    Atualizando

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,14 +1,17 @@
 import Versions from './components/Versions'
 import icons from './assets/icons.svg'
 import Form from './components/Form'
+import Dices from './components/Dices/Dices'
 
 
 function App() {
-  return ( <div>
-  <h1>Helo word</h1>
-        <Form label1={'Graduação'} label2={'Ano'} textButton={'Salvar'} placeholder1= {'engearia'} placeholder2={'2021'}></Form>
-        <Form label1={'Nome'} label2={'idade'} textButton={'Salvar'} placeholder1= {'thiago'} placeholder2={'22'}></Form>
-        </div>
+  return ( 
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Jogo Dos dados</h1>
+      </header>
+      <Dices />
+          </div>
   )
 }
 
